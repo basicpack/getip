1c7a648aed4d66c0155018de458e37b5485cd017
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-04-16 16:22:59 -0300

    Create getip.php

---
+++ getip.php
@@ -0,0 +1,14 @@
+<?php
+//Anderson Ismael
+//14 de novembro de 2018
+
+function getIp(){
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
